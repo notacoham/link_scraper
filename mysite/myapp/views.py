0bdec72c45affaea84471ace8a804b158714552f
notacoham <alexcottam12@gmail.com>
2025-07-28 15:44:07 -0600

    add first view, template, model, and url pattern for basic web-scrape functionality

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from bs4 import BeautifulSoup
+import requests
+from .models import Link
 # Create your views here.
+
+def scrape(request):
+    page = requests.get('https://www.facebook.com')
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    for link in soup.find_all('a'):
+        link_address = link.get('href')
+        link_text = link.string
+        Link.objects.create(address=link_address, name=link_text)
+    
+    data = Link.objects.all()
+
+    return render(request, 'myapp/result.html', {'data': data})
